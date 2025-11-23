f9532b656128f92b3a816f185b0d9adac12cc2de
Ludens2038 <hariskekic86@gmail.com>
2025-11-23 15:26:11 +0100

    implemented movement for beaker

--- visionOSProjekt/ImmersiveView.swift
+++ visionOSProjekt/ImmersiveView.swift
@@ -1,26 +1,41 @@
-//
-//  ImmersiveView.swift
-//  visionOSProjekt
-//
-//  Created by Kekic Haris - s2310237010 on 21.11.25.
-//
-
 import SwiftUI
 import RealityKit
 import RealityKitContent
+import Spatial
 
 struct ImmersiveView: View {
-
+    @State private var startPosition: SIMD3<Float>? = nil
+    
     var body: some View {
         RealityView { content in
-            // Add the initial RealityKit content
             if let immersiveContentEntity = try? await Entity(named: "Immersive", in: realityKitContentBundle) {
                 content.add(immersiveContentEntity)
-
-                // Put skybox here.  See example in World project available at
-                // https://developer.apple.com/
             }
         }
+        .gesture(
+            DragGesture()
+                .targetedToAnyEntity()
+                .onChanged { value in
+                    let entity = value.entity
+                    let t = value.translation
+                    
+                    if startPosition == nil {
+                        startPosition = entity.position
+                    }
+                    
+                    guard let start = startPosition else { return }
+                    
+                    let factor: Float = 0.001
+                    let newX = start.x + Float(t.width)  * factor
+                    let newZ = start.z + Float(-t.height) * factor
+                    
+                    entity.position.x = newX
+                    entity.position.z = newZ
+                }
+                .onEnded { _ in
+                    startPosition = nil
+                }
+        )
     }
 }
 
