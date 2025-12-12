2c823135fb0fc8a64ad05836763febfaa2d19355
Ludens2038 <hariskekic86@gmail.com>
2025-12-12 11:09:03 +0100

    before adding finish

---
+++ Clock/ClockBundle.swift
@@ -0,0 +1,16 @@
+//
+//  ClockBundle.swift
+//  Clock
+//
+//  Created by Haris Kekic on 12.12.25.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct ClockBundle: WidgetBundle {
+    var body: some Widget {
+        Clock()
+    }
+}
